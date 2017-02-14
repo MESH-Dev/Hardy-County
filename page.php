271261f2d453cd7fc16592bd16108bb0732d0325
MESH Laptop <mesh@MESHs-MacBook-Pro.local>
2017-02-14 13:52:02 -0500

    Banner and heading

--- page.php
+++ page.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
 
 <main id="content">
+	<div class="banner interior" style="background-image: url('http://localhost:8888/hardy/wp-content/uploads/2017/02/narrow-banner-placeholder.png');">
+		<div class="banner-heading">
+			<div class="container">
+				<p>Optional Parent Page</p>
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
+					<h1><?php the_title(); ?></h1>
+
+				<?php endwhile; ?>	
+			</div>		
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="columns-9">
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-					<h1><?php the_title(); ?></h1>
 
 					<?php the_content(); ?>
 
