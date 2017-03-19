5c4a2b0702f5d6892c7f8166c4e1f3fe1ec315a0
meshdesign <joshdodd@meshfresh.com>
2017-03-19 13:03:28 -0400

    admin cleanup

--- functions/clean.php
+++ functions/clean.php
@@ -13,14 +13,14 @@ add_action('login_head', 'my_custom_login_logo'); //Add custom logo to admin
 //Clean up Dashboard
 function remove_dashboard_widgets(){
 
-    //remove_meta_box('dashboard_right_now','dashboard','core'); // right now overview box
-    //remove_meta_box('dashboard_incoming_links','dashboard','core'); // incoming links box
-    //remove_meta_box('dashboard_quick_press','dashboard','core'); // quick press box
+    remove_meta_box('dashboard_right_now','dashboard','core'); // right now overview box
+    remove_meta_box('dashboard_incoming_links','dashboard','core'); // incoming links box
+    remove_meta_box('dashboard_quick_press','dashboard','core'); // quick press box
     //remove_meta_box('dashboard_plugins','dashboard','core'); // new plugins box
-    //remove_meta_box('dashboard_recent_drafts','dashboard','core'); // recent drafts box
-    //remove_meta_box('dashboard_recent_comments','dashboard','core'); // recent comments box
-    //remove_meta_box('dashboard_primary','dashboard','core'); // wordpress development blog box
-    //remove_meta_box('dashboard_secondary','dashboard','core'); // other wordpress news box
+    remove_meta_box('dashboard_recent_drafts','dashboard','core'); // recent drafts box
+    remove_meta_box('dashboard_recent_comments','dashboard','core'); // recent comments box
+    remove_meta_box('dashboard_primary','dashboard','core'); // wordpress development blog box
+    remove_meta_box('dashboard_secondary','dashboard','core'); // other wordpress news box
 }
 
 // Remove menus froms the admin area
@@ -29,17 +29,41 @@ function delete_menu_items() {
     /*** Remove menu http://codex.wordpress.org/Function_Reference/remove_menu_page
     syntaxe : remove_menu_page( $menu_slug )  **/
     //remove_menu_page('index.php'); // Dashboard
-    //remove_menu_page('edit.php'); // Posts
+    remove_menu_page('edit.php'); // Posts
     //remove_menu_page('upload.php'); // Media
     //remove_menu_page('link-manager.php'); // Links
     //remove_menu_page('edit.php?post_type=page'); // Pages
-    //remove_menu_page('edit-comments.php'); // Comments
+    remove_menu_page('edit-comments.php'); // Comments
     //remove_menu_page('themes.php'); // Appearance
     //remove_menu_page('plugins.php'); // Plugins
     //remove_menu_page('users.php'); // Users
     //remove_menu_page('tools.php'); // Tools
     //remove_menu_page('options-general.php'); // Settings
 }
+
+/*********************
+* re-order left admin menu
+**********************/
+function reorder_admin_menu( $__return_true ) {
+    return array(
+        'index.php', // Dashboard
+        'edit.php?post_type=page', // Pages 
+        'edit.php?post_type=listing',
+        'edit.php?post_type=event',
+        'upload.php', // Media
+        'separator1', // --Space--
+        'separator2', // --Space--
+        'themes.php', // Appearance
+ 
+         
+ 
+   );
+}
+add_filter( 'custom_menu_order', 'reorder_admin_menu' );
+add_filter( 'menu_order', 'reorder_admin_menu' );
+
+
+
 
 //Security and header clean-ups
 remove_action( 'wp_head', 'wlwmanifest_link');
