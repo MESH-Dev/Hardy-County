fbf3f25af95f61de2935f13acf950d277d0613e3
Shaun <shaun@meshfresh.com>
2017-06-14 18:04:46 -0400

    Final updates

--- functions/clean.php
+++ functions/clean.php
@@ -31,7 +31,7 @@ function delete_menu_items() {
     //remove_menu_page('index.php'); // Dashboard
     remove_menu_page('edit.php'); // Posts
     //remove_menu_page('upload.php'); // Media
-    //remove_menu_page('link-manager.php'); // Links
+    remove_menu_page('link-manager.php'); // Links
     //remove_menu_page('edit.php?post_type=page'); // Pages
     remove_menu_page('edit-comments.php'); // Comments
     //remove_menu_page('themes.php'); // Appearance
